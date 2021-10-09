0231f2a66ce9976e7afec78150ded4f24f448ea8
Diego Amsa <diamsa03@gmail.com>
2021-10-09 18:51:09 -0400

    dividing data by class

--- spam.js
+++ spam.js
@@ -20,7 +20,19 @@ fs.readFile("./myOwnData.csv", (err, data) => {
         dataInArray[i][3],
       ]);
     }
+    //console.log(fourRow);
 
-    console.log(fourRow);
+    function divideDataByClass(parsedData) {
+      let shortOrLargeNames = [[], []];
+      for (let i = 1; i < parsedData.length; i++) {
+        if (parsedData[i][0].length > 6) {
+          shortOrLargeNames[0].push(parsedData[i][0]);
+        } else {
+          shortOrLargeNames[1].push(parsedData[i][0]);
+        }
+      }
+      return shortOrLargeNames;
+    }
+    console.log(divideDataByClass(fourRow));
   }
 });
